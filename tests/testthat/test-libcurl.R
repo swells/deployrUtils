b31f3d139b091381c95ee1401b43441ab9763b16
Sean Wells <sean.wells.sc@gmail.com>
2015-08-21 04:30:04 -0700

    inital commit

--- tests/testthat/test-libcurl.R
+++ tests/testthat/test-libcurl.R
@@ -7,5 +7,11 @@ context("checkpoint existence of libcurl")
 describe("https", {
   it("can download over https", { 
     expect_equal(TRUE, TRUE)
+	#library(checkpoint)
+	options(download.file.method = "wget", url.method = "wget")
+	
+	mran <- "http://mran.revolutionanalytics.com/snapshot/" 
+	mran.root <- url(mran, method = "wget")
+	readLines(mran.root)
   })
 })
